ca3b5cf7f5754ba08505922124354d8788ea9a14
KonradKruczek <240720@edu.p.lodz.pl>
2023-04-14 17:31:46 +0200

    8.2 Poprawa @OverRide

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -3,7 +3,7 @@ package edu.kis.vh.nursery;
 public class FIFORhymer extends defaultCountingOutRhymer {
 
     public final defaultCountingOutRhymer temp = new defaultCountingOutRhymer();
-
+    @Override
     public int countOut() {
         while (!callCheck())
             temp.countIn(super.countOut());
