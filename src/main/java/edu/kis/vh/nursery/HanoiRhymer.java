5898c850c18a1cb03d55ddc2ca9a8b8c2ae58fc8
Mephir12 <piorunas10@gmail.com>
2023-05-17 19:44:40 +0200

    17. Dodanie dokumentacji klasy HanoiRhymer

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,12 +1,25 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Klassa HanoiRhymer śledzi ilość odrzuconych elementów.
+ * Rozszerza klasę defaultCountingOutRhymer.
+ */
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
     private int totalRejected = 0;
 
+    /**
+     * @return Metoda reportRejected zwraca ilość odrzuconych elementów.
+     */
     public int reportRejected() {
         return totalRejected;
     }
+
+    /**
+     * @param in Metoda countIn inkrementuje wartość odrzuconych elementów, jeżeli rhymer nie jest pusty
+     *           i wartośc liczby jest większa od ostatnio dodanego elementu.
+     *           W przeciwnym wypadku element zostaje dodany do rhymera używając metody klasy nadrzędnej countIn.
+     */
     @Override
     public void countIn(int in) {
         if (!callCheck() && in > peekaboo())
